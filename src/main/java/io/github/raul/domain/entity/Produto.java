b20e64dadc4bec7aede8cd7d3734087b65c6c478
Raul <raul.cfr112@gmail.com>
2024-03-06 08:25:02 -0400

    Adicionado internaciolização

--- src/main/java/io/github/raul/domain/entity/Produto.java
+++ src/main/java/io/github/raul/domain/entity/Produto.java
@@ -19,11 +19,11 @@ public class Produto {
     @Column(name = "id")
     private Integer id;
 
-    @NotEmpty(message = "Campo descrição é obrigatório")
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     @Column(name = "descricao")
     private String descricao;
 
-    @NotNull(message = "Campo preço é obrigatório")
+    @NotNull(message = "{campo.preco.obrigatorio}")
     @Column(name = "preco_unitario")
     private BigDecimal preco;
 
