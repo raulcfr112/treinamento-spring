742e710358f60ca09748f4e52c857df51919ef80
Raul <raul.cfr112@gmail.com>
2024-03-07 12:31:29 -0400

    Adicionado build, Deploy e Profiles Maven para builds diferentes

--- src/main/java/io/github/raul/VendasApplication.java
+++ src/main/java/io/github/raul/VendasApplication.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
